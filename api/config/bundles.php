a5b6846100118a0cdd78b5536f26a978282097fd
YummYume <yummyume@users.noreply.github.com>
2023-10-09 15:35:10 +0200

    Add Sentry to front and API

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
