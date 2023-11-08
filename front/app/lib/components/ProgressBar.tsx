bb086a792a32bb83c5b6b5c80a25ad4321f1b1ad
YummYume <yummyume@users.noreply.github.com>
2023-11-08 15:40:29 +0100

    WIP try to fix progress bar

--- front/app/lib/components/ProgressBar.tsx
+++ front/app/lib/components/ProgressBar.tsx
@@ -1,6 +1,52 @@
 import { useNProgress } from '@tanem/react-nprogress';
 import { useMemo } from 'react';
 
+export type ContainerProps = {
+  /**
+   * The current progress of the progress bar.
+   */
+  progress: number;
+  /**
+   * Whether or not the progress bar is finished.
+   */
+  visible: boolean;
+  /**
+   * The duration of the animation.
+   */
+  animationDuration?: number;
+  /**
+   * The minimum progress possible for the progress bar.
+   */
+  minimum?: number;
+  /**
+   * The message to display to screen readers when the progress bar is active.
+   */
+  loadingMessage?: string;
+  /**
+   * The children to render.
+   */
+  children?: React.ReactNode;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export type BarProps = {
+  /**
+   * The current progress of the progress bar.
+   */
+  progress: number;
+  /**
+   * Whether or not the progress bar is finished.
+   */
+  visible: boolean;
+  /**
+   * The duration of the animation.
+   */
+  animationDuration?: number;
+  /**
+   * The children to render.
+   */
+  children?: React.ReactNode;
+} & React.HTMLAttributes<HTMLDivElement>;
+
 export type ProgressBarProps = {
   /**
    * Whether or not the progress bar is active (visible).
@@ -23,10 +69,68 @@ export type ProgressBarProps = {
    */
   minimum?: number;
   /**
-   * The classes to apply to the progress bar.
+   * The props to spread on the bar element.
    */
-  barClassName?: string;
+  barProps?: React.HTMLAttributes<HTMLDivElement>;
+  /**
+   * The container component.
+   */
+  container?: (props: ContainerProps) => React.ReactElement;
+  /**
+   * The bar component.
+   */
+  bar?: (props: BarProps) => React.ReactElement;
+  /**
+   * The children to render.
+   */
+  children?: React.ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
+
+export const Container = ({
+  progress,
+  visible,
+  animationDuration = 300,
+  loadingMessage = 'Loading...',
+  minimum = 0,
+  children,
+  ...props
+}: ContainerProps) => (
+  <div
+    role="progressbar"
+    aria-valuemin={minimum}
+    aria-valuemax={100}
+    aria-valuenow={progress}
+    aria-hidden={!visible}
+    aria-valuetext={visible ? loadingMessage : undefined}
+    style={{
+      opacity: visible ? 1 : 0,
+      pointerEvents: 'none',
+      transition: `opacity ${animationDuration}ms linear`,
+    }}
+    {...props}
+  >
+    {children}
+  </div>
+);
+
+export const Bar = ({
+  progress,
+  visible,
+  animationDuration = 300,
+  children,
+  ...props
+}: BarProps) => (
+  <div
+    style={{
+      width: `${progress}%`,
+      opacity: visible ? 1 : 0,
+      transitionDuration: `${animationDuration}ms`,
+    }}
+    {...props}
+  >
+    {children}
+  </div>
+);
 
 export const ProgressBar = ({
   active,
@@ -34,7 +138,13 @@ export const ProgressBar = ({
   animationDuration = 300,
   incrementDuration = 750,
   minimum = 0.1,
-  barClassName = 'h-full bg-blue-600 transition-all motion-reduce:transition-none ease-in-out dark:bg-blue-500',
+  barProps = {
+    className:
+      'h-full bg-blue-600 transition-all motion-reduce:transition-none ease-in-out dark:bg-blue-500',
+  },
+  container: ContainerComponent = Container,
+  bar: BarComponent = Bar,
+  children,
   ...props
 }: ProgressBarProps) => {
   const {
@@ -55,23 +165,21 @@ export const ProgressBar = ({
   }, [progress, isFinished]);
 
   return (
-    <div
-      role="progressbar"
-      aria-valuemin={minimum * 100}
-      aria-valuemax={100}
-      aria-valuenow={barStatus.progress}
-      aria-hidden={!barStatus.visible}
-      aria-valuetext={barStatus.visible ? loadingMessage : undefined}
+    <ContainerComponent
       {...props}
+      progress={barStatus.progress}
+      visible={barStatus.visible}
+      animationDuration={duration}
+      loadingMessage={loadingMessage}
+      minimum={minimum}
     >
-      <div
-        className={barClassName}
-        style={{
-          width: `${barStatus.progress}%`,
-          opacity: barStatus.visible ? 1 : 0,
-          transitionDuration: `${duration}ms`,
-        }}
-      ></div>
-    </div>
+      <BarComponent
+        {...barProps}
+        progress={barStatus.progress}
+        visible={barStatus.visible}
+        animationDuration={duration}
+      />
+      {children}
+    </ContainerComponent>
   );
 };
