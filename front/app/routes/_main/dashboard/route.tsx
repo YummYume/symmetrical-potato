d70c0e9b0b83c0546c90e3b08ec9eb5e8a86eced
LeoBruant <bruantleo@gmail.com>
2023-12-06 16:21:05 +0100

    Fix : Loader
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- front/app/routes/_main/dashboard/route.tsx
+++ front/app/routes/_main/dashboard/route.tsx
@@ -1,16 +1,6 @@
 import { Heading, Section } from '@radix-ui/themes';
 import { useTranslation } from 'react-i18next';
 
-import { denyAccessUnlessGranted } from '~utils/security.server';
-
-import type { DataFunctionArgs } from '@remix-run/node';
-
-export async function loader({ context }: DataFunctionArgs) {
-  denyAccessUnlessGranted(context.user);
-}
-
-export type Loader = typeof loader;
-
 export default function Dashboard() {
   const { t } = useTranslation();
 
