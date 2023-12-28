2d4cd853ad5242dd5b5fa6f5fd12971ea82ec4fe
LeoBruant <bruantleo@gmail.com>
2023-12-28 18:45:24 +0100

    Feat : Improvements
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- E2E/playwright.config.ts
+++ E2E/playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   use: {
-    baseURL: 'http://symmetrical-potato.com',
+    baseURL: process.env.BASE_URL ?? 'http://symmetrical-potato.com',
   },
 };
 
