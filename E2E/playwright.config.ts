5c9438a43b256608e29c4c2076922a489bafafff
LeoBruant <bruantleo@gmail.com>
2023-12-29 15:29:07 +0100

    Feat : Changes
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- E2E/playwright.config.ts
+++ E2E/playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  reporter: process.env.CI ? 'dot' : 'list',
   use: {
     baseURL: process.env.BASE_URL ?? 'http://symmetrical-potato.com',
   },
