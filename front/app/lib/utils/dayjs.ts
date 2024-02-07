361f5b18b2e2f0d3cdfcc760d22fc1de2ee75ee9
YummYume <yummyume@users.noreply.github.com>
2024-02-08 00:26:46 +0100

    Fix dashboard

--- front/app/lib/utils/dayjs.ts
+++ front/app/lib/utils/dayjs.ts
@@ -6,6 +6,10 @@ import relativeTime from 'dayjs/plugin/relativeTime.js';
 import timezone from 'dayjs/plugin/timezone.js';
 import utc from 'dayjs/plugin/utc.js';
 
+// Locales
+import 'dayjs/locale/en';
+import 'dayjs/locale/fr';
+
 // Plugins
 dayjs.extend(utc);
 dayjs.extend(timezone);
